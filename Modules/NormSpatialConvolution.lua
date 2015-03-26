933aeafadda3a73a301c9a03efc0726400103114
Ross Goroshin <rgoroshin@gmail.com>
2015-03-26 15:10:18 -0400

    added test_ConvLISTA.lua

---
+++ Modules/NormSpatialConvolution.lua
@@ -0,0 +1,123 @@
+local NormSpatialConvolution, parent = torch.class('nn.NormSpatialConvolution', 'nn.Module')
+
+function NormSpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padding)
+   parent.__init(self)
+
+   dW = dW or 1
+   dH = dH or 1
+
+   self.nInputPlane = nInputPlane
+   self.nOutputPlane = nOutputPlane
+   self.kW = kW
+   self.kH = kH
+
+   self.dW = dW
+   self.dH = dH
+   self.padding = padding or 0
+
+   self.weight = torch.Tensor(nOutputPlane, nInputPlane, kH, kW)
+   self.bias = torch.Tensor(nOutputPlane)
+   self.gradWeight = torch.Tensor(nOutputPlane, nInputPlane, kH, kW)
+   self.gradBias = torch.Tensor(nOutputPlane)
+   
+   self:reset()
+   self:norm_filters()  
+end
+
+function NormSpatialConvolution:norm_filters() 
+    local w = self.weight 
+    local wsz = w:size()
+    w = w:transpose(1,2):contiguous():resize(wsz[2],wsz[1]*wsz[3]*wsz[4]) 
+    local norm = w:norm(2,2):expandAs(w):contiguous() 
+    w:cdiv(norm)
+    self.weight:copy(w:resize(wsz[2],wsz[1],wsz[3],wsz[4]):transpose(1,2):contiguous())   
+    collectgarbage() 
+end
+
+function NormSpatialConvolution:reset(stdv)
+   if stdv then
+      stdv = stdv * math.sqrt(3)
+   else
+      stdv = 1/math.sqrt(self.kW*self.kH*self.nInputPlane)
+   end
+   if nn.oldSeed then
+      self.weight:apply(function()
+         return torch.uniform(-stdv, stdv)
+      end)
+      self.bias:apply(function()
+         return torch.uniform(-stdv, stdv)
+      end) 
+   else
+      self.weight:uniform(-stdv, stdv)
+      self.bias:uniform(-stdv, stdv)
+   end
+end
+
+local function backCompatibility(self)
+   self.finput = self.finput or self.weight.new()
+   self.fgradInput = self.fgradInput or self.weight.new()
+   self.padding = self.padding or 0
+   if self.weight:dim() == 2 then 
+      self.weight = self.weight:view(self.nOutputPlane, self.nInputPlane, self.kH, self.kW)
+   end
+   if self.gradWeight:dim() == 2 then 
+      self.gradWeight = self.gradWeight:view(self.nOutputPlane, self.nInputPlane, self.kH, self.kW)
+   end
+end
+
+local function makeContiguous(self, input, gradOutput)
+   if not input:isContiguous() then
+      self._input = self._input or input.new()
+      self._input:resizeAs(input):copy(input)
+      input = self._input
+   end
+   if gradOutput then
+      if not gradOutput:isContiguous() then
+	 self._gradOutput = self._gradOutput or gradOutput.new()
+	 self._gradOutput:resizeAs(gradOutput):copy(gradOutput)
+	 gradOutput = self._gradOutput
+      end
+   end
+   return input, gradOutput
+end
+
+-- function to re-view the weight layout in a way that would make the MM ops happy
+local function viewWeight(self)
+   self.weight = self.weight:view(self.nOutputPlane, self.nInputPlane * self.kH * self.kW)
+   self.gradWeight = self.gradWeight:view(self.nOutputPlane, self.nInputPlane * self.kH * self.kW)
+end
+
+local function unviewWeight(self)
+   self.weight = self.weight:view(self.nOutputPlane, self.nInputPlane, self.kH, self.kW)
+   self.gradWeight = self.gradWeight:view(self.nOutputPlane, self.nInputPlane, self.kH, self.kW)
+end
+
+function NormSpatialConvolution:updateOutput(input)
+   self:norm_filters()  
+   backCompatibility(self)
+   viewWeight(self)
+   input = makeContiguous(self, input)
+   local out = input.nn.SpatialConvolutionMM_updateOutput(self, input)
+   unviewWeight(self)
+   return out
+end
+
+function NormSpatialConvolution:updateGradInput(input, gradOutput)
+   if self.gradInput then
+      backCompatibility(self)
+      viewWeight(self)
+      input, gradOutput = makeContiguous(self, input, gradOutput)
+      local out = input.nn.SpatialConvolutionMM_updateGradInput(self, input, gradOutput)
+      unviewWeight(self)
+      return out
+   end
+end
+
+function NormSpatialConvolution:accGradParameters(input, gradOutput, scale)
+   backCompatibility(self)
+   input, gradOutput = makeContiguous(self, input, gradOutput)
+   viewWeight(self)
+   local out = input.nn.SpatialConvolutionMM_accGradParameters(self, input, gradOutput, scale)
+   unviewWeight(self)
+   return out
+end
